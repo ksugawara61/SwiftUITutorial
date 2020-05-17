a5b72c56767e7a8587e98684582ecd67939bc5c8
ksugawara61 <katsuya@kokohadoko.net>
2020-05-17 21:47:40 +0900

    [add] handling user input

---
+++ Landmarks/Model/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by 菅原勝也 on 2020/05/17.
+//  Copyright © 2020 listadoko. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject  {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
