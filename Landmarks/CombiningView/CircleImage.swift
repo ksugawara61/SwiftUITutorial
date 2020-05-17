a5f87973b269da2392aee4a3a2a8e44b04837b16
ksugawara61 <katsuya@kokohadoko.net>
2020-05-17 18:24:12 +0900

    [add] Combining View tutorial

---
+++ Landmarks/CombiningView/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 菅原勝也 on 2020/05/17.
+//  Copyright © 2020 listadoko. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("operator-icon-11")
+            .resizable()
+            .frame(width: 200.0, height: 200.0, alignment: .leading)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
