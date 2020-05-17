ada765cf699e743f07590307041f1bb4cc38256f
ksugawara61 <katsuya@kokohadoko.net>
2020-05-17 17:46:03 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 菅原勝也 on 2020/05/17.
+//  Copyright © 2020 listadoko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
